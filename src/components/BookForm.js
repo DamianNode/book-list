643ceff9ca3d01062795d9d76bfb11b236d3bdd4
Damian Janus <damian.janus@gmail.com>
2021-05-07 00:58:44 +0200

    Refactor to use reducer

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,10 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { BookContext } from '../contexts/BookContext';
 
 const BookForm = () => {
-    const { addBook, removeBook } = useContext(BookContext);
-    return (
+    const { dispatch } = useContext(BookContext);
+    const [ title, setTitle ] = useState('');
+    const [ author, setAuthor ] = useState('');
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        dispatch({type: 'ADD_BOOK', book: {title, author}});
+        setTitle('')
+        setAuthor('')
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="title">Title</label>
+            <input 
+                type="text" 
+                id="title"
+                value={title}
+                placeholder="add title"
+                onChange={(e) => setTitle(e.target.value)} 
+                required
+            />
+
+            <label htmlFor="author">Author</label>
+            <input 
+                type="text" 
+                id="author"
+                value={author}
+                placeholder="add author"
+                onChange={(e) => setAuthor(e.target.value)}
+                required
+            />
+            <button type="submit">Add book</button>
+        </form>
     )
 }
 
--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,29 +1,13 @@
-import React, { createContext, useState } from 'react';
-import { v4 as uuid } from 'uuid';
+import React, { createContext, useReducer } from 'react';
+import { BookReducer } from '../reducers/BookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = ({children}) => {
-    const [books, setBooks] = useState([
-        { title: 'the way of kings', author: 'patrick', id: uuid() },
-        { title: 'the name of the wind', author: 'malcolm', id: uuid() },
-        { title: 'the final empire', author: 'johnatan', id: uuid() },
-    ])
-
-    const addBook = ({title, author}) => {
-        const newBook = {
-            title, author, id: uuid()
-        }
-        setBooks([...books, newBook])
-    }
-
-    const removeBook = (id) => {
-        const newBooks = books.filter(book => book.id !== id)
-        setBooks(newBooks)
-    }
+    const [books, dispatch] = useReducer(BookReducer, [])
 
     return (
-        <BookContext.Provider value={{books, addBook, removeBook}}>
+        <BookContext.Provider value={{books, dispatch}}>
             {children}
         </BookContext.Provider>
     )
